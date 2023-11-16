f643e862402d2797c12d33c5c45f738dfa5199ab
Dhrumil Patel <98255642+itechdp@users.noreply.github.com>
2023-11-16 14:38:13 +0530

    main.py

--- main.py
+++ main.py
@@ -15,6 +15,23 @@ WorkFlow:
 
 7. Handeling Date and Time Data --> convert date and time into numerical format
 '''
+'''
+WorkFlow: 
+
+1. Data Cleaning --> Handeling missing values, dealing with outliers and correcting data entry errors.
+
+2. Data Integration --> Merging of multiple dataset in one single dataset
+
+3. Data Transformation --> normalization and standardization log or power transformation
+
+4. Feature selection --> Reduce the dimensionality and improve model performance. Correlation analysis, statiscal tests or feature importance analysis
+
+5. Handeling imbalanced data --> Resampleing , oversampling or undersampleing 
+
+6. Handling Text and categorical data --> tokenization, one hot encoder, label encoding 
+
+7. Handeling Date and Time Data --> convert date and time into numerical format
+'''
 
 import streamlit as sl
 import pandas as pd
@@ -22,21 +39,23 @@ import pandas as pd
 class DatasetReading:
     def get_file(self):
 
-        sl.write("<h1 style='text-align:Center'>Automated Data Processing</h1>",unsafe_allow_html=True)
-        self.file = sl.text_input(label="Give File Path:")
+        sl.write("<h1 style='text-align:Center'>Automated Data Pre-Processing</h1>",unsafe_allow_html=True)
+        self.file = sl.file_uploader("Upload a file", type=["csv", "xlsx", "xls"])
         self.file_name = sl.text_input(label='Save File With Name As:')
-        if '.csv' in self.file:
-            self.dataset = pd.read_csv(self.file)
-            sl.subheader("Dataset Overview")
-            sl.dataframe(self.dataset.head())
-            return self.dataset
-        elif '.xlsx' in self.file:
-            self.dataset = pd.read_excel(self.file)
-            sl.subheader("Dataset Overview")
-            sl.dataframe(self.dataset.head())
-            return self.dataset
-        else:
-            sl.warning("Invalid File Extension or File Not Found")
+
+        if self.file is not None:
+            if self.file.name.endswith('.csv'):
+                self.dataset = pd.read_csv(self.file)
+                sl.subheader("Dataset Overview")
+                sl.dataframe(self.dataset.head())
+                return self.dataset
+            elif self.file.name.endswith('.xlsx'):
+                self.dataset = pd.read_excel(self.file)
+                sl.subheader("Dataset Overview")
+                sl.dataframe(self.dataset.head())
+                return self.dataset
+            else:
+                sl.warning("Invalid File Extension or File Not Found")
     
     def get_df_info(self):
         df_columns = self.dataset.columns
@@ -68,7 +87,7 @@ class DataHandeling(DatasetReading):
         sl.subheader("Handeling Missing Values")
         updated_dataset = self.dataset
         from sklearn.impute import SimpleImputer
-        imputer = SimpleImputer(strategy='median')
+        imputer = SimpleImputer(strategy='mean')
         updated_dataset = imputer.fit_transform(updated_dataset) 
         self.dataset = pd.DataFrame(updated_dataset,index=self.dataset.index,columns=self.dataset.columns) 
         sl.dataframe(self.dataset.head())  
@@ -95,13 +114,12 @@ class DataHandeling(DatasetReading):
     def save_file(self):
         sl.subheader("Processed Dataset")
         sl.dataframe(self.dataset.head())
-        self.get_df_info()
-        if '.csv' in self.file:
-            self.dataset.to_csv(f"{self.file_name} Processed.csv",index=True)
-
+        if self.file.name.endswith(".csv"):
+            sl.download_button(label='Download Processed File',data=self.dataset.to_csv(index=True),file_name=f"{self.file.name.replace('.csv','')} Processed File.csv",key='download_csv')
             return self.dataset
+        
         elif '.xlsx' in self.file:
-            self.dataset.to_excel(f'{self.file_name} Processed.xlsx',index=True)
+            sl.download_button(label='Download Processed File',data=self.dataset.to_excel(index=True),file_name=f"{self.file.name.replace('.xlsx','')} Processed File.xlsx",key='download_csv')
             return self.dataset
         
 
